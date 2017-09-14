51ccac695e9de856eeef6255892f3c760550e76c
julianaclp <julianaclp350@gmail.com>
2017-09-14 13:18:10 -0300

    Criação da função que soma as entradas elevadas ao quadrado

--- functions.vhd
+++ functions.vhd
@@ -6,6 +6,8 @@ USE work.common.all;
 PACKAGE functions IS 
 	FUNCTION sqr(input: SIGNED(size-1 DOWNTO 0))
 		RETURN SIGNED;
+	FUNCTION inputSum(iReal, iComplex: SIGNED(size*2-1 DOWNTO 0))
+		RETURN SIGNED;
 END functions;
 
 PACKAGE BODY functions IS 
@@ -15,5 +17,12 @@ PACKAGE BODY functions IS
 		inputSquared := input*input;
 	RETURN inputSquared;
 	END sqr;
+	
+	FUNCTION inputSum(iReal, iComplex: SIGNED(size*2-1 DOWNTO 0)) RETURN SIGNED IS
+	variable sumOutput: SIGNED(size*2-1 DOWNTO 0);
+	BEGIN
+		sumOutput := iReal + iComplex;
+	RETURN sumOutput;
+	END inputSum;
 END functions;
 	
--- somador.vhd
+++ somador.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 USE work.common.all;
 USE work.functions.all;
-
+USE work.function_calls.all;
 
 ENTITY somador IS 
 	PORT(uReal, uComplex, uPastReal, uPastComplex: IN SIGNED (size-1 DOWNTO 0);
@@ -11,16 +11,18 @@ ENTITY somador IS
 			outReal, outComplex, outPastReal, outPastComplex: OUT SIGNED (size*2-1 DOWNTO 0));
 END somador;
 
-
 ARCHITECTURE somador OF somador IS
 BEGIN
 	PROCESS(clk)
+	variable realTemp, complexTemp, pastRealTemp, pastComplexTemp: SIGNED(size*2-1 DOWNTO 0);
 	BEGIN
 		IF(clk'EVENT AND clk='1') THEN
-			outReal <= sqr(uReal);
-			outComplex <= sqr(uComplex);
-			outPastComplex <= sqr(uPastComplex);
-			outPastReal <= sqr(uPastReal);
+			squaredInputCall(uReal, uComplex, uPastReal, uPastComplex, realTemp, complexTemp, pastRealTemp, pastComplexTemp);
+			inputSumCall(realTemp, complexTemp, pastRealTemp, pastComplexTemp, realTemp, complexTemp);
+			outReal <= realTemp;
+			outComplex <= complexTemp;
+			outPastReal <= pastRealTemp;
+			outPastComplex <= pastComplexTemp;
 		END IF;
 	END PROCESS;
 END somador; 
