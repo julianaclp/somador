a3591efd5f10f3fb6263249353f417d546bef725
julianaclp <julianaclp350@gmail.com>
2017-09-26 12:14:27 -0300

    Otimização da função que calcula o módulo da entrada

--- functions.vhd
+++ functions.vhd
@@ -4,26 +4,18 @@ USE ieee.numeric_std.all;
 USE work.common.all;
 
 PACKAGE functions IS 
-	-- FUNCTIONS --
-	FUNCTION sqr(input: SIGNED(size-1 DOWNTO 0))
-		RETURN SIGNED;
-	FUNCTION inputSum(iReal, iComplex: SIGNED(size*2-1 DOWNTO 0))
+	FUNCTION absolute(iReal, iComplex: SIGNED(size-1 DOWNTO 0)) 
 		RETURN SIGNED;
 END functions;
 
 PACKAGE BODY functions IS 
-	FUNCTION sqr(input: SIGNED(size-1 DOWNTO 0)) RETURN SIGNED IS
-	variable inputSquared: SIGNED(size*2-1 DOWNTO 0); 
+	FUNCTION absolute(iReal, iComplex: SIGNED(size-1 DOWNTO 0)) RETURN SIGNED IS
+	variable temp: SIGNED(size*2-1 DOWNTO 0);
+	variable output: SIGNED(size-1 DOWNTO 0);
 	BEGIN
-		inputSquared := input*input;
-	RETURN inputSquared;
-	END sqr;
-	
-	FUNCTION inputSum(iReal, iComplex: SIGNED(size*2-1 DOWNTO 0)) RETURN SIGNED IS
-	variable sumOutput: SIGNED(size*2-1 DOWNTO 0);
-	BEGIN
-		sumOutput := iReal + iComplex;
-	RETURN sumOutput;
-	END inputSum;
+		temp := (iReal*iReal) + (iComplex*iComplex);
+		output := temp(size*2-1 DOWNTO size);
+	RETURN output;
+	END absolute;
 END functions;
 	
--- somador.vhd
+++ somador.vhd
@@ -9,24 +9,19 @@ USE work.lookuptable.all;
 ENTITY somador IS 
 	PORT(uReal, uComplex, uPastReal, uPastComplex: IN SIGNED (size-1 DOWNTO 0);
 			clk: IN STD_LOGIC;
-			outReal, outComplex, outPastReal, outPastComplex: OUT SIGNED (size*2-1 DOWNTO 0));
+			outPresent, outPast: OUT SIGNED (size-1 DOWNTO 0));
 END somador;
 
 ARCHITECTURE somador OF somador IS
 BEGIN
 	PROCESS(clk)
-	variable realTemp, complexTemp, pastRealTemp, pastComplexTemp, sumPresent, sumPast: SIGNED(size*2-1 DOWNTO 0);
-	variable realPresent, complexPresent, realPast, complexPast: SIGNED(size-1 DOWNTO 0);
+	variable sumPresent, sumPast: SIGNED(size-1 DOWNTO 0);
 	BEGIN
 		IF(clk'EVENT AND clk='1') THEN
-			realTemp := sqr(uReal);
-			complexTemp := sqr(uComplex);
-			pastRealTemp := sqr(uPastReal);
-			pastComplexTemp := sqr(uPastComplex);
-			sumPresent := inputSum(realTemp, complexTemp);
-			sumPast := inputSum(pastRealTemp, pastComplexTemp);
-			lut(sumPresent, realPresent, complexPresent);
-			lut(sumPast, realPast, complexPast);
+			outPresent <= absolute(uReal, uComplex);
+			outPast <= absolute(uPastReal, uPastComplex);
+--			lut(sumPresent, realPresent, complexPresent);
+--			lut(sumPast, realPast, complexPast);
 		END IF;
 	END PROCESS;
 END somador; 
