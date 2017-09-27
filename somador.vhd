35c3bede05375334d5a7fd55f87b7854e0a3c218
julianaclp <julianaclp350@gmail.com>
2017-09-27 09:50:09 -0300

    Criando processos diferentes para cada operação

--- somador.vhd
+++ somador.vhd
@@ -3,26 +3,40 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 USE work.common.all;
 USE work.functions.all;
-USE work.function_calls.all;
 USE work.lookuptable.all;
 
 ENTITY somador IS 
 	PORT(uReal, uComplex, uPastReal, uPastComplex: IN SIGNED (size-1 DOWNTO 0);
 			clk: IN STD_LOGIC;
-			outPresent, outPast: OUT SIGNED (size-1 DOWNTO 0));
+			outComplex, outReal: OUT SIGNED (size DOWNTO 0) := (OTHERS => 'X'));
 END somador;
 
 ARCHITECTURE somador OF somador IS
+	SIGNAL sumPresent, sumPast: SIGNED(size-1 DOWNTO 0);
 BEGIN
-	PROCESS(clk)
-	variable sumPresent, sumPast: SIGNED(size-1 DOWNTO 0);
+	absoluteProcess: PROCESS(clk)
+	variable tempSum, tempSumPast: SIGNED(size-1 DOWNTO 0);
 	BEGIN
 		IF(clk'EVENT AND clk='1') THEN
-			outPresent <= absolute(uReal, uComplex);
-			outPast <= absolute(uPastReal, uPastComplex);
---			lut(sumPresent, realPresent, complexPresent);
---			lut(sumPast, realPast, complexPast);
+			tempSum := absolute(uReal, uComplex);
+			tempSumPast := absolute(uPastReal, uPastComplex);
+			sumPresent <= tempSum;
+			sumPast <= tempSumPast;
 		END IF;
-	END PROCESS;
+	END PROCESS absoluteProcess;
+
+	lutProcess: PROCESS(clk)
+	variable realPresent, complexPresent: SIGNED(size DOWNTO 0);
+	variable tempSum, tempSumPast: SIGNED(size-1 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempSum := sumPresent;
+			tempSumPast := sumPast;
+			lut(tempSum, realPresent, complexPresent);
+	--		lut(tempSumPast, realPast, complexPast);
+			outComplex <= complexPresent;
+			outReal <= realPresent;
+		END IF;
+	END PROCESS lutProcess;
 END somador; 
  
