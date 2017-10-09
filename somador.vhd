6f4673bdb92d68ef3374e995ebd339991c5c1fe0
julianaclp <julianaclp350@gmail.com>
2017-10-09 10:51:08 -0300

    Criação de outros process para serem executadas em paralelo

--- somador.vhd
+++ somador.vhd
@@ -6,37 +6,106 @@ USE work.functions.all;
 USE work.lookuptable.all;
 
 ENTITY somador IS 
-	PORT(uReal, uComplex, uPastReal, uPastComplex: IN SIGNED (size-1 DOWNTO 0);
+	PORT(uReal, uComplex, uPastReal, uPastComplex: IN STD_LOGIC_VECTOR(size-1 DOWNTO 0);
 			clk: IN STD_LOGIC;
-			outComplex, outReal: OUT SIGNED (size DOWNTO 0) := (OTHERS => 'X'));
+--			outComplex, outReal, outPastReal, outPastComplex: OUT STD_LOGIC_VECTOR(size DOWNTO 0) := (OTHERS => 'X');
+			sumPresent, 
+			sumPast: BUFFER SIGNED(size-1 DOWNTO 0);
+			outComplex, 
+			outReal,
+			outPastReal, 
+			outPastComplex: BUFFER SIGNED(size DOWNTO 0)
+			);
 END somador;
 
 ARCHITECTURE somador OF somador IS
-	SIGNAL sumPresent, sumPast: SIGNED(size-1 DOWNTO 0);
+SIGNAL signedReal, signedComplex, signedPastReal, signedPastComplex: SIGNED(size-1 DOWNTO 0);
 BEGIN
-	absoluteProcess: PROCESS(clk)
-	variable tempSum, tempSumPast: SIGNED(size-1 DOWNTO 0);
+	stage1: PROCESS(clk)
 	BEGIN
 		IF(clk'EVENT AND clk='1') THEN
-			tempSum := absolute(uReal, uComplex);
-			tempSumPast := absolute(uPastReal, uPastComplex);
+			signedReal <= signed(uReal);
+			signedComplex <= signed(uComplex);
+			signedPastReal <= signed(uPastReal);
+			signedPastComplex <= signed(uPastComplex); 
+		END IF;
+	END PROCESS stage1;
+	stage2Present: PROCESS(clk)
+	variable tempSum: SIGNED(size-1 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempSum := absolute(signedReal, signedComplex);
 			sumPresent <= tempSum;
+		END IF;
+	END PROCESS stage2Present;
+	
+	stage2Past: PROCESS(clk)
+	variable tempSumPast: SIGNED(size-1 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempSumPast := absolute(signedPastReal, signedPastComplex);
 			sumPast <= tempSumPast;
 		END IF;
-	END PROCESS absoluteProcess;
+	END PROCESS stage2Past;
 
-	lutProcess: PROCESS(clk)
+	stage3Present: PROCESS(clk)
 	variable realPresent, complexPresent: SIGNED(size DOWNTO 0);
-	variable tempSum, tempSumPast: SIGNED(size-1 DOWNTO 0);
+	variable tempSum: SIGNED(size-1 DOWNTO 0);
 	BEGIN
 		IF(clk'EVENT AND clk='1') THEN
 			tempSum := sumPresent;
-			tempSumPast := sumPast;
 			lut(tempSum, realPresent, complexPresent);
-	--		lut(tempSumPast, realPast, complexPast);
 			outComplex <= complexPresent;
 			outReal <= realPresent;
 		END IF;
-	END PROCESS lutProcess;
+	END PROCESS stage3Present;
+	
+	stage3Past: PROCESS(clk)
+	variable realPast, complexPast: SIGNED(size DOWNTO 0);
+	variable tempSumPast: SIGNED(size-1 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempSumPast := sumPast;
+			lut(tempSumPast, realPast, complexPast);
+			outPastReal <= realPast;
+			outPastComplex <= complexPast;
+		END IF;
+	END PROCESS stage3Past;
+	
+	stage4PresentReal: PROCESS(clk) 
+	variable tempReal, tempComplex: SIGNED(size*2 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempReal := outReal*signedReal;
+			tempComplex := outComplex*signedComplex;
+		END IF;
+	END process stage4PresentReal;
+	
+	stage4PresentComplex: PROCESS(clk) 
+	variable tempReal, tempComplex: SIGNED(size*2 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempReal := outReal*signedComplex;
+			tempComplex := outComplex*signedReal;
+		END IF;
+	END process stage4PresentComplex;
+	
+	stage4PastReal: PROCESS(clk) 
+	variable tempPastReal, tempPastComplex: SIGNED(size*2 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempPastReal := outPastReal*signedPastReal;
+			tempPastComplex := outPastComplex*signedPastComplex;
+		END IF;
+	END process stage4PastReal;
+	
+	stage4PastComplex: PROCESS(clk) 
+	variable tempPastReal, tempPastComplex: SIGNED(size*2 DOWNTO 0);
+	BEGIN
+		IF(clk'EVENT AND clk='1') THEN
+			tempPastReal := outPastReal*signedPastComplex;
+			tempPastComplex := outPastComplex*signedPastReal;
+		END IF;
+	END process stage4PastComplex;
 END somador; 
  
